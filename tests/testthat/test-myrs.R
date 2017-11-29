8fa19504ab59c983b79608731ccad25c33bc458b
Rory Nolan <rorynoolan@gmail.com>
2017-11-29 11:33:30 +0000

    Fix `test_myrs` for AppVeyor.

--- tests/testthat/test-myrs.R
+++ tests/testthat/test-myrs.R
@@ -3,11 +3,13 @@ context("myrs")
 test_that("myrpois works", {
   expect_true(list(myrpois(-5:5, 10)) %in%
                 list(c(-2, -5, -6, -2, 0, 0, 1, 0, 4, 6, 5),
-                     c(-6, -3, -6, -2, -1, 0, 0, 1, 0, 5, 6)))  # travis
+                     c(-6, -3, -6, -2, -1, 0, 0, 1, 0, 5, 6),  # travis
+                     c(-5, -2, -1, -6, -2, 0, 0, 0, 3, 4, 5)))  # appveyor
 })
 
 test_that("myrbern works", {
   expect_true(list(myrbern(seq(0.01, 0.9, length.out = 7), 12)) %in%
               list(c(0, 0, 1, 0, 0, 1, 1),
-                   c(0, 0, 0, 1, 1, 1, 1)))  # travis
+                   c(0, 0, 0, 1, 1, 1, 1),  # travis
+                   c(0, 0, 0, 1, 1, 1, 1)))
 })
--- tests/testthat/test_best_tau.R
+++ tests/testthat/test_best_tau.R
@@ -1,7 +1,7 @@
 context("Automatic tau finding")
 
 test_that("best_tau works", {
-  skip_on_windows()
+  skip_on_os("windows")
   img <- ijtiff::read_tif(system.file("extdata", "bleached.tif",
                                       package = "detrendr"))[, , 1, ]
   expect_true(round(best_tau(img, seed = 0, parallel = 2)) %in% c(32, 34))
