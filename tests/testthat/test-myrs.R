c4bd25713f5643f1e59c3e9582203e7de7d025a7
Rory Nolan <rorynoolan@gmail.com>
2017-11-29 11:12:28 +0000

    Skip certain tests on Windows.

--- tests/testthat/test-myrs.R
+++ tests/testthat/test-myrs.R
@@ -1,16 +1,12 @@
 context("myrs")
 
 test_that("myrpois works", {
-  skip_on_appveyor()
-  skip_on_cran()
   expect_true(list(myrpois(-5:5, 10)) %in%
                 list(c(-2, -5, -6, -2, 0, 0, 1, 0, 4, 6, 5),
                      c(-6, -3, -6, -2, -1, 0, 0, 1, 0, 5, 6)))  # travis
 })
 
 test_that("myrbern works", {
-  skip_on_appveyor()
-  skip_on_cran()
   expect_true(list(myrbern(seq(0.01, 0.9, length.out = 7), 12)) %in%
               list(c(0, 0, 1, 0, 0, 1, 1),
                    c(0, 0, 0, 1, 1, 1, 1)))  # travis
