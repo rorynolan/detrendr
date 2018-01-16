ec27826d14af9bc81022b9da10fe3eeb4e186aa3
Rory Nolan <rorynoolan@gmail.com>
2018-01-16 13:22:29 +0000

    Reduce the size of the example image.

--- tests/testthat/test_detrend.R
+++ tests/testthat/test_detrend.R
@@ -4,29 +4,28 @@ img <- ijtiff::read_tif(system.file("extdata", "bleached.tif",
 test_that("detrending works", {
   context("Boxcar detrending")
   corrected <- img_detrend_boxcar(img, "auto", seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected[, , 1, ])), 2), 1.85,
+  expect_equal(round(mean(brightness_pillars(corrected[, , 1, ])), 2), 1.45,
                tolerance = 0.05)
   corrected10 <- img_detrend_boxcar(img, 10, seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected10[, , 1, ])), 2), 1.25)
+  expect_equal(round(mean(brightness_pillars(corrected10[, , 1, ])), 2), 1.27)
   corrected50 <- img_detrend_boxcar(img, 50, seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected50[, , 1, ])), 2), 2.23)
+  expect_equal(round(mean(brightness_pillars(corrected50[, , 1, ])), 2), 2.08)
   corrected100 <- img_detrend_boxcar(img, 100, seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected100[, , 1, ])), 2), 3.34)
+  expect_equal(round(mean(brightness_pillars(corrected100[, , 1, ])), 2), 3.04)
   corrected300 <- img_detrend_boxcar(img, 300, seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected300[, , 1, ])), 2), 8.16)
+  expect_equal(round(mean(brightness_pillars(corrected300[, , 1, ])), 2), 5.6)
   context("Exponential filtering detrending")
   corrected <- img_detrend_exp(img, "auto", seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected[, , 1, ])), 2), 2.09,
+  expect_equal(round(mean(brightness_pillars(corrected[, , 1, ])), 2), 1.64,
                tolerance = 0.05)
   corrected10 <- img_detrend_exp(img, 10, seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected10[, , 1, ])), 2), 1.28)
+  expect_equal(round(mean(brightness_pillars(corrected10[, , 1, ])), 2), 1.29)
   corrected50 <- img_detrend_exp(img, 50, seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected50[, , 1, ])), 2), 2.55)
+  expect_equal(round(mean(brightness_pillars(corrected50[, , 1, ])), 2), 2.38)
   corrected100 <- img_detrend_exp(img, 100, seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected100[, , 1, ])), 2), 3.95)
+  expect_equal(round(mean(brightness_pillars(corrected100[, , 1, ])), 2), 3.38)
   corrected1000 <- img_detrend_exp(img, 1000, seed = 0, parallel = 2)
-  expect_equal(round(mean(brightness_pillars(corrected1000[, , 1, ])), 2),
-               10.36)
+  expect_equal(round(mean(brightness_pillars(corrected1000[, , 1, ])), 2), 6.01)
   context("Polynomial detrending")
   expect_warning(img_detrend_polynom(img, "auto", seed = 0, parallel = 2),
                  "polynomial degree")
@@ -34,19 +33,19 @@ test_that("detrending works", {
                                                     seed = 0, parallel = 2))
   expect_equal(round(mean(brightness_pillars(corrected[, , 1, ]),
                          na.rm = TRUE), 2),
-               2.12, tolerance = 0.15)
+               1.6, tolerance = 0.15)
   corrected1 <- img_detrend_polynom(img, 1, seed = 0, parallel = 2)
   expect_equal(round(mean(brightness_pillars(corrected1[, , 1, ]),
-                          na.rm = TRUE), 1), 5.0)
+                          na.rm = TRUE), 1), 4.5)
   corrected2 <- img_detrend_polynom(img, 2, seed = 0, parallel = 2)
   expect_equal(round(mean(brightness_pillars(corrected2[, , 1, ]),
-                          na.rm = TRUE), 1), 3.9)
+                          na.rm = TRUE), 1), 3.5)
   corrected4 <- img_detrend_polynom(img, 4, seed = 0, parallel = 2)
   expect_equal(round(mean(brightness_pillars(corrected4[, , 1, ]),
-                          na.rm = TRUE), 1), 2.8)
+                          na.rm = TRUE), 1), 2.6)
   corrected8 <- img_detrend_polynom(img, 8, seed = 0, parallel = 2)
   expect_equal(round(mean(brightness_pillars(corrected8[, , 1, ]),
-                          na.rm = TRUE), 1), 2.0)
+                          na.rm = TRUE), 1), 1.9)
 })
 
 context("Detrending errors")
