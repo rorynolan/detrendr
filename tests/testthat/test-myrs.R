b1344c601b98391b9dda78c2b8760a4a3ea3f259
Rory Nolan <rorynoolan@gmail.com>
2019-05-30 11:50:05 +0100

    Remove superfluous lines.

--- tests/testthat/test-myrs.R
+++ tests/testthat/test-myrs.R
@@ -10,9 +10,7 @@ test_that("myrpois works on linux", {
   skip_if_not(get_os() == "linux")
   set.seed(1)
   x <- myrpois(-5:5)
-  if (getRversion() >= "3.6") {
-    expect_equal(x, c(-6, -6, -5, -1, -3, 0, 1, 2, 4, 4, 4))
-  }
+  expect_equal(x, c(-6, -6, -5, -1, -3, 0, 1, 2, 4, 4, 4))
 })
 
 test_that("myrpois works on windows", {
