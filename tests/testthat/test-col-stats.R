d3412dde1769b964b699aff252aa13c92f248228
Rory Nolan <rorynoolan@gmail.com>
2023-01-28 01:41:29 +0000

    Attempt a smidge more test coverage.

--- tests/testthat/test-col-stats.R
+++ tests/testthat/test-col-stats.R
@@ -2,3 +2,10 @@ test_that("mean_cols works", {
   m <- matrix(sample.int(100), ncol = 20)
   expect_equal(mean_cols(m), apply(m, 2, mean))
 })
+
+test_that("brightness_cols edge case works", {
+  expect_equal(
+    brightness_cols(matrix(numeric(0), ncol = 7)),
+    rep(NaN, 7)
+  )
+})
